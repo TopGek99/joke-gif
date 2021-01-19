234068ea49658649c295d0893b5d76ee2df3fc89
TopGek99 <arowe890@gmail.com>
2021-01-19 20:45:38 +1030

    added directories

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,15 @@
 var searchTerm = $("#search-text");
 var searchButton = $("#searchButton");
+var cityName = $("#city-name");
+var language = $("#language");
+
+var beerURL = "https://api.openbrewerydb.org/breweries?by_city="+cityName;
+var languageURL = "https://api.cloudmersive.com/nlp-v2/translate/language/deu/to/eng";
+$.post(languageURL, {
+    TextToTranslate: "string"
+  }).done(function(response) {
+      alert(response);
+  });
 
 // initilizing materialize css 
 $(document).ready(function () {

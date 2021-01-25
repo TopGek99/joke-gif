7f98dc3335b0ca854e113d6f15030ae4a20e798d
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-01-25 19:22:30 +1030

    html & js edited

--- javascript/script.js
+++ javascript/script.js
@@ -1,59 +1,14 @@
-var searchTerm = $("#search-text");
+// var searchTerm = $("#search-text");
 var searchButton = $("#searchButton");
-var cityName = $("#city-name");
-var language = $("#language");
-
-// var beerURL = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
-// var languageURL = "https://api.cloudmersive.com/nlp-v2/translate/language/deu/to/eng";
-// $.post(languageURL, {
-//     TextToTranslate: "string"
-// }).done(function (response) {
-//     alert(response);
-// });
-
-// initilizing materialize css 
-$(document).ready(function () {
-    M.AutoInit();
-});
-
-// searching for breweries 
-searchButton.on("click", function () {
-    // event.preventDefault();
-    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + searchTerm.val().trim()
-    console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response);
-
-        for (let i = 0; i < 5; i++) {
-            var breweryName = $("<h2>");
-            breweryName.text(response[i].name);
-            $(".breweryList").append(breweryName);
-
-            var breweryType = $("<p>");
-            breweryType.text("Brewery Type: " + response[i].brewery_type);
-            $(".breweryList").append(breweryType);
-
-            var street = $("<p>");
-            street.text("Address: " + response[i].street);
-            $(".breweryList").append(street);
-
-            var website = $("<p>");
-            website.text(response[i].website_url);
-            $(".breweryList").append(website);
-        }
-    })
-});
 
 // Joke api
-var category = $("#category");
+var category = $("#input_text");
 
-searchButton.on("click", function () {
-    // event.preventDefault();
-    var queryURL = "https://v2.jokeapi.dev/joke/" + category + "?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=" + type
-    console.log(queryURL);
+searchButton.on("click", function (event) {
+    event.preventDefault();
+    var queryURL = "https://v2.jokeapi.dev/joke/Any?contains=" + category.val(); 
+    // console.log(queryURL);
+    console.log(category);
     $.ajax({
         url: queryURL,
         method: "GET",

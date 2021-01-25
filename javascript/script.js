bcf1635e006689404c17bb8c061b2274e8626255
TopGek99 <arowe890@gmail.com>
2021-01-25 20:46:16 +1030

    added local storage

--- javascript/script.js
+++ javascript/script.js
@@ -1,52 +1,50 @@
 // var searchTerm = $("#search-text");
 var searchButton = $("#searchButton");
+var resetButton = $("#reset");
+var searchList;
 var isJoke;
 
 // Joke api
 var category = $("#input_text");
 
+$(document).ready(function() {
+    if (localStorage.getItem("searches") == null) {
+        searchList = [];
+        localStorage.setItem("searches",JSON.stringify(searchList));
+    } else {
+        searchList = localStorage.getItem("searches");
+    }
+})
+
 searchButton.on("click", function (event) {
     event.preventDefault();
+    searchList.push(category.val());
+    localStorage.setItem("searches",JSON.stringify(searchList));
     var queryURL = "https://v2.jokeapi.dev/joke/Any?contains=%20" + category.val() + "%20";
-    // console.log(queryURL);
-    // console.log(category);
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        console.log(response);
+        $(".jokeSearched").empty();
 
         isJoke = !response.error;
         if (isJoke) {
-            $(".jokeSearched").append(response.joke);
-            
+            if (response.type == "twopart") {
+                $(".jokeSearched").append(response.setup);
+                $(".jokeSearched").append("\n"+response.delivery);
+            } else {
+                $(".jokeSearched").append(response.joke);
+            }
         } else {
             $("form").append($("<p>").text("No joke found! Please try again"));
         }
-        
-
-
-
-        // Will need to have the user chose one of these, cannot figure out at this stage how to do both
-        // if type = single
-        // console.log(response.joke);
-
-        // // if type = twopart
-        // console.log(response.setup);
-        // console.log(response.delivery);
-
-        // search string 
-        // need to append "&contains=" + searchterm to end of queryURL 
-
-        // amount of jokes 
-        // need to append "&amount=" + number to end of queryURL 
 
         var gifURL = "https://api.giphy.com/v1/gifs/search?q=" + category.val() + "&api_key=dc6zaTOxFJmzC";
         $.ajax({
             url: gifURL,
             method: "GET"
         }).then(function (response) {
-            console.log(isJoke);
+            $(".gipHy").empty();
             if (isJoke) {
                 var imageUrl = response.data[0].images.fixed_height.url;
 
@@ -59,3 +57,10 @@ searchButton.on("click", function (event) {
         });
     });
 });
+
+resetButton.on("click", function(event) {
+    event.preventDefault();
+    $(".gipHy").empty();
+    $(".jokeSearched").empty();
+    category.val("");
+})

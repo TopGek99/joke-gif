10f17aa09f07747695323e09921e37b0ad417697
TopGek99 <arowe890@gmail.com>
2021-01-25 17:26:10 +1030

    attempted implementation of joke api

--- javascript/script.js
+++ javascript/script.js
@@ -3,56 +3,51 @@ var searchButton = $("#searchButton");
 var cityName = $("#city-name");
 var language = $("#language");
 
-// var beerURL = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
-// var languageURL = "https://api.cloudmersive.com/nlp-v2/translate/language/deu/to/eng";
-// $.post(languageURL, {
-//     TextToTranslate: "string"
-// }).done(function (response) {
-//     alert(response);
-// });
-
 // initilizing materialize css 
 $(document).ready(function () {
     M.AutoInit();
 });
 
 // searching for breweries 
-searchButton.on("click", function () {
-    // event.preventDefault();
-    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + searchTerm.val().trim()
-    console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response);
+// searchButton.on("click", function () {
+//     event.preventDefault();
+//     var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + searchTerm.val().trim()
+//     console.log(queryURL);
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//     }).then(function (response) {
+//         console.log(response);
 
-        for (let i = 0; i < 5; i++) {
-            var breweryName = $("<h2>");
-            breweryName.text(response[i].name);
-            $(".breweryList").append(breweryName);
+//         for (let i = 0; i < 5; i++) {
+//             var breweryName = $("<h2>");
+//             breweryName.text(response[i].name);
+//             $(".breweryList").append(breweryName);
 
-            var breweryType = $("<p>");
-            breweryType.text("Brewery Type: " + response[i].brewery_type);
-            $(".breweryList").append(breweryType);
+//             var breweryType = $("<p>");
+//             breweryType.text("Brewery Type: " + response[i].brewery_type);
+//             $(".breweryList").append(breweryType);
 
-            var street = $("<p>");
-            street.text("Address: " + response[i].street);
-            $(".breweryList").append(street);
+//             var street = $("<p>");
+//             street.text("Address: " + response[i].street);
+//             $(".breweryList").append(street);
 
-            var website = $("<p>");
-            website.text(response[i].website_url);
-            $(".breweryList").append(website);
-        }
-    })
-});
+//             var website = $("<p>");
+//             website.text(response[i].website_url);
+//             $(".breweryList").append(website);
+//         }
+//     })
+// });
 
 // Joke api
-var category = $("#category");
+// var category = $("#category");
 
-searchButton.on("click", function () {
-    // event.preventDefault();
-    var queryURL = "https://v2.jokeapi.dev/joke/" + category + "?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=" + type
+
+searchButton.on("click", function (event) {
+    event.preventDefault();
+    var category = $("input[name='category']:checked").val();
+    var type = $("input[name='type']:checked").val();
+    var queryURL = "https://v2.jokeapi.dev/joke/" + category + "?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=" + type;
     console.log(queryURL);
     $.ajax({
         url: queryURL,

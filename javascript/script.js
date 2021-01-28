79ec7ac99211cd3aae2516b91717a9c1c70060b0
TopGek99 <arowe890@gmail.com>
2021-01-28 16:35:18 +1030

    restructured js plus added some features

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,14 @@
-// var searchTerm = $("#search-text");
 var searchButton = $("#searchButton");
 var resetButton = $("#reset");
+var jokeSearched = $(".jokeSearched");
+var searchTerm = $("#input_text");
+var gifDiv = $(".gipHy");
+var historyDiv = $("#history");
 var searchList;
 var isJoke;
 
 // Joke api
-var category = $("#input_text");
+
 
 $(document).ready(function() {
     if (localStorage.getItem("searches") == null) {
@@ -14,30 +17,47 @@ $(document).ready(function() {
     } else {
         searchList = JSON.parse(localStorage.getItem("searches"));
         for (var i=0;i<searchList.length;i++) {
-
+            var hButton = createHistoryButton(searchList[i]);
+            historyDiv.append(hButton);
         }
     }
 })
 
 searchButton.on("click", function (event) {
     event.preventDefault();
-    searchList.push(category.val());
+    searchList.push(searchTerm.val());
     localStorage.setItem("searches",JSON.stringify(searchList));
-    var queryURL = "https://v2.jokeapi.dev/joke/Any?contains=%20" + category.val() + "%20";
+    showJoke(searchTerm.val(),false);
+});
+
+resetButton.on("click", function(event) {
+    event.preventDefault();
+    gifDiv.empty();
+    $(".jokeSearched").empty();
+    searchTerm.val("");
+});
+
+function showJoke(keyword, isButton) {
+    var queryURL = "https://v2.jokeapi.dev/joke/Any?contains=%20" + keyword + "%20&blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
     $.ajax({
         url: queryURL,
         method: "GET",
-    }).then(function (response) {
-        $(".jokeSearched").empty();
+    }).then(function (jokeResponse) {
+        jokeSearched.empty();
 
-        isJoke = !response.error;
+        isJoke = !jokeResponse.error;
         if (isJoke) {
-            if (response.type == "twopart") {
-                $(".jokeSearched").append(response.setup);
-                $(".jokeSearched").append("\n"+response.delivery);
+            if (jokeResponse.type == "twopart") {
+                jokeSearched.append(jokeResponse.setup);
+                jokeSearched.append("\n"+jokeResponse.delivery);
             } else {
-                $(".jokeSearched").append(response.joke);
+                jokeSearched.append(jokeResponse.joke);
             }
+            if (!isButton) {
+                var hButton = createHistoryButton(keyword);
+                historyDiv.append(hButton);
+            }
+            showGIF(keyword);
         } else {
             var errorMessage = $("<p>").text("No joke found! Please try again");
             $("form").append(errorMessage);
@@ -45,29 +65,32 @@ searchButton.on("click", function (event) {
                 errorMessage.remove();
             },1000);
         }
-
-        var gifURL = "https://api.giphy.com/v1/gifs/search?q=" + category.val() + "&api_key=dc6zaTOxFJmzC";
-        $.ajax({
-            url: gifURL,
-            method: "GET"
-        }).then(function (response) {
-            $(".gipHy").empty();
-            if (isJoke) {
-                var imageUrl = response.data[0].images.fixed_height.url;
-
-                var gif = $("<img>");
-
-                gif.attr("src", imageUrl);
-                $(".gipHy").append(gif);
-            }
-            
-        });
     });
-});
+}
 
-resetButton.on("click", function(event) {
-    event.preventDefault();
-    $(".gipHy").empty();
-    $(".jokeSearched").empty();
-    category.val("");
-})
+function showGIF(searchword) {
+    var gifURL = "https://api.giphy.com/v1/gifs/random?tag=" + searchword + "&api_key=OZnHKN9d6fdfIOqVaero2nouKah6hqfX";
+    $.ajax({
+        url: gifURL,
+        method: "GET"
+    }).then(function (giphyResponse) {
+        gifDiv.empty();
+        var imageUrl = giphyResponse.data.images.fixed_height.url;
+        console.log(giphyResponse.data.images.fixed_height.url);
+
+        var gif = $("<img>");
+
+        gif.attr("src", imageUrl);
+        gifDiv.append(gif);
+    });
+}
+
+function createHistoryButton(btnText) {
+    newHistoryButton = $("<button>");
+    newHistoryButton.text(btnText);
+    newHistoryButton.addClass("btn");
+    newHistoryButton.on("click",function(){
+        showJoke(btnText,true);
+    });
+    return newHistoryButton;
+}
